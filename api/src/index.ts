2a8298b4681085050c359889f20f5d4716af802c
Andrew Durnford <andrewdurnford3@gmail.com>
2022-01-22 21:05:48 +1100

    chore: Add apollo server api

---
+++ api/src/index.ts
@@ -0,0 +1,21 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => {
+      return "Hello World";
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
