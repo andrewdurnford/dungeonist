d6044907fdff027bc4131153bd05809011e946b4
Andrew Durnford <andrewdurnford3@gmail.com>
2022-01-23 22:18:41 +1100

    chore: Setup graphql codegen

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,23 +1,60 @@
 import { PrismaClient } from "@prisma/client";
 import { ApolloServer, gql } from "apollo-server";
+import * as bcrypt from "bcrypt";
+import { IResolvers } from "./utils/types";
 
 const typeDefs = gql`
+  type Mutation {
+    signup(input: SignupInput!): User
+  }
+
   type Query {
-    hello: String
+    users: [User!]!
+  }
+
+  input SignupInput {
+    email: String!
+    password: String!
+  }
+
+  type User {
+    id: ID!
+    email: String!
   }
 `;
 
-const resolvers = {
+const resolvers: IResolvers = {
   Query: {
-    hello: () => {
-      return "Hello World";
+    users: async (_, {}, ctx) => {
+      return await ctx.prisma.user.findMany();
+    },
+  },
+
+  Mutation: {
+    signup: async (_, { input: { email, password } }, ctx) => {
+      let user = await ctx.prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (user) throw new Error("Email is already taken");
+
+      user = await ctx.prisma.user.create({
+        data: {
+          email: email,
+          password: await bcrypt.hash(password, 10),
+        },
+      });
+
+      return user;
     },
   },
 };
 
 const prisma = new PrismaClient();
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, context: { prisma } });
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
