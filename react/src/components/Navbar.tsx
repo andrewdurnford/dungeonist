98d82eec02772f152e67329e8a12943a0427d3cd
Andrew Durnford <andrewdurnford3@gmail.com>
2022-01-25 18:34:30 +1100

    feat: Add user logout

--- react/src/components/Navbar.tsx
+++ react/src/components/Navbar.tsx
@@ -2,23 +2,44 @@ import { Link } from "react-router-dom";
 import { useMeQuery } from "../utils/graphql";
 
 function Navbar() {
-  const { data, loading } = useMeQuery();
+  const { data, loading, refetch } = useMeQuery();
 
   if (loading) return <p>Loading...</p>;
+
+  const isLoggedIn = !!data?.me;
 
   return (
     <nav>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        {data?.me && <li>Welcome {data.me.email}!</li>}
+        {isLoggedIn ? (
+          <>
+            <li>Welcome {data?.me?.email}!</li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li
+              onClick={() => {
+                localStorage.removeItem("token");
+                // TODO: Update to clear user context
+                refetch();
+              }}
+            >
+              Logout
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
