db171d366b7baffeaf7e20b1022264ea245c6534
Andrew Durnford <andrewdurnford3@gmail.com>
2022-01-25 17:16:45 +1100

    feat(react): Add user signup form

--- react/src/components/Signup.tsx
+++ react/src/components/Signup.tsx
@@ -1,5 +1,43 @@
+import { gql, useMutation } from "@apollo/client";
+import { useForm } from "react-hook-form";
+
+const SIGNUP = gql`
+  mutation Signup($input: SignupInput!) {
+    signup(input: $input) {
+      id
+      email
+    }
+  }
+`;
+
+type SignupInput = {
+  email: string;
+  password: string;
+};
+
 function Signup() {
-  return <h1>Signup</h1>;
+  const [signup, { loading, error }] = useMutation(SIGNUP, {
+    onError: (err) => console.error(err),
+  });
+  const { register, handleSubmit } = useForm<SignupInput>();
+  const onSubmit = handleSubmit(({ email, password }) =>
+    signup({ variables: { input: { email, password } } })
+  );
+
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="email">Email</label>
+        <input type="text" id="email" {...register("email")} />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" {...register("password")} />
+        <button type="submit">Signup</button>
+      </form>
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: "red" }}>Error: {error.message}</p>}
+    </div>
+  );
 }
 
 export default Signup;
--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,5 +1,25 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
