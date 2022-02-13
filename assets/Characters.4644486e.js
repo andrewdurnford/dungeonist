dd4dc5c85ca77ad68e0a02eba1b8169aca9a64f0
andrewdurnford <38198147+andrewdurnford@users.noreply.github.com>
2022-02-13 05:58:54 +0000

    Deploying to gh-pages from  @ 42c3385d44b5c2fb1c5a280a6c859dc65ba60b30 🚀

--- assets/Characters.4644486e.js
+++
@@ -1,1 +0,0 @@
-import{v as e}from"./vendor.c913c43d.js";const c=[e.query("Characters",(t,r,a)=>r(a.data({characters:[{id:"1",name:"Obi-Wan Kenobi",__typename:"Character"}]})))];export{c as handlers};
---
+++ assets/Me.c561375f.js
@@ -0,0 +1,1 @@
+import{v as a}from"./vendor.ff8afe3d.js";const l=[a.query("Me",(m,e,r)=>e(r.data({me:{id:"1",email:"luke.skywalker@gmail.com",__typename:"User"},__typename:"Query"})))];export{l as handlers};
