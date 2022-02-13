2c48909e11985bdc0e973ae3c4a3f8af071829a7
andrewdurnford <38198147+andrewdurnford@users.noreply.github.com>
2022-02-13 04:45:40 +0000

    Deploying to gh-pages from  @ 5f8faf3198a39e875f1f7a7ed3b6265e392c83a4 🚀

---
+++ assets/Characters.4644486e.js
@@ -0,0 +1,1 @@
+import{v as e}from"./vendor.c913c43d.js";const c=[e.query("Characters",(t,r,a)=>r(a.data({characters:[{id:"1",name:"Obi-Wan Kenobi",__typename:"Character"}]})))];export{c as handlers};
